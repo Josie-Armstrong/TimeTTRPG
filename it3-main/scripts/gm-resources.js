7534b684bd7ea3d2e2b4161aec1391b312ed7dc4
Josie-Armstrong <156936598+Josie-Armstrong@users.noreply.github.com>
2026-03-30 14:46:50 -0600

    roll dice on GM page, worked on char slots

--- it3-main/scripts/gm-resources.js
+++ it3-main/scripts/gm-resources.js
@@ -1,1 +1,174 @@
-char_slots
+let char1 = {
+    "valid_sheet": true,
+
+    "header": {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: 0
+    },
+
+    "wounds": {
+        0: false, 1: false, 2: false, 3: false
+    },
+
+    "abilities": {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+    },
+
+    "eccentricities": "",
+
+    "expertise": "",
+
+    "background": "",
+
+    "gear": "",
+
+    "notes": ""
+}
+
+let char2 = {
+    "valid_sheet": true,
+
+    "header": {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: 0
+    },
+
+    "wounds": {
+        0: false, 1: false, 2: false, 3: false
+    },
+
+    "abilities": {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+    },
+
+    "eccentricities": "",
+
+    "expertise": "",
+
+    "background": "",
+
+    "gear": "",
+
+    "notes": ""
+}
+
+let char3 = {
+    "valid_sheet": true,
+
+    "header": {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: 0
+    },
+
+    "wounds": {
+        0: false, 1: false, 2: false, 3: false
+    },
+
+    "abilities": {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+    },
+
+    "eccentricities": "",
+
+    "expertise": "",
+
+    "background": "",
+
+    "gear": "",
+
+    "notes": ""
+}
+
+let char4 = {
+    "valid_sheet": true,
+
+    "header": {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: 0
+    },
+
+    "wounds": {
+        0: false, 1: false, 2: false, 3: false
+    },
+
+    "abilities": {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+    },
+
+    "eccentricities": "",
+
+    "expertise": "",
+
+    "background": "",
+
+    "gear": "",
+
+    "notes": ""
+}
+
+let char5 = {
+    "valid_sheet": true,
+
+    "header": {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: 0
+    },
+
+    "wounds": {
+        0: false, 1: false, 2: false, 3: false
+    },
+
+    "abilities": {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+    },
+
+    "eccentricities": "",
+
+    "expertise": "",
+
+    "background": "",
+
+    "gear": "",
+
+    "notes": ""
+}
+
+// Stores all characters
+let char_arr = [char1, char2, char3, char4, char5];
+
+// VALUES char name, sturdy, ability, eccentricities, and wounds
